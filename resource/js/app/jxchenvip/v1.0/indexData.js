2ddf9b52a6280f4b7ceb7c3adbe0ec58c85af78b
jxchen <jxchenvip@163.com>
2016-08-09 16:31:44 +0800

    update

--- resource/js/app/jxchenvip/v1.0/indexData.js
+++ resource/js/app/jxchenvip/v1.0/indexData.js
@@ -7,7 +7,8 @@ define(function(require, exports, module) {
         { id: 4, link: "./project/javascript/upload_images/index.html", innerText: "上传图片" },
         { id: 5, link: "./project/javascript/zeptoPhotoClip/index.html", innerText: "图片裁剪" },
         { id: 6, link: "./project/javascript/Waterfal_flow/index.html", innerText: "瀑布流" },
-        { id: 7, link: "javascript:;", innerText: "敬请期待" }
+        { id: 7, link: "./project/react/demo1/index.html", innerText: "react-demo" },
+        { id: 8, link: "javascript:;", innerText: "敬请期待" }
     ];
     module.exports.data = indexDate;
 })
