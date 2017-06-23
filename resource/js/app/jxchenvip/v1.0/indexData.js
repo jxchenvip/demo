701cda951d74a4e7d4d35ff3143a80766b66dcbc
jxchen <jxchenvip@163.com>
2017-06-23 17:47:25 +0800

    add demo

--- resource/js/app/jxchenvip/v1.0/indexData.js
+++ resource/js/app/jxchenvip/v1.0/indexData.js
@@ -1,19 +1,20 @@
 define(function(require, exports, module) {
     var indexDate = [
-        { id: 0, link: "./project/canvas/tablet/index.html", innerText: "写字板", top: "true" },
-        { id: 1, link: "./project/canvas/circel_progress/index.html", innerText: "圆形进度条" },
-        { id: 2, link: "./project/javascript/data_select/index.html", innerText: "日期联动下拉" },
-        { id: 3, link: "./project/javascript/countDown/index.html", innerText: "倒计时" },
-        { id: 4, link: "./project/javascript/upload_images/index.html", innerText: "上传图片" },
-        { id: 5, link: "./project/javascript/zeptoPhotoClip/index.html", innerText: "图片裁剪" },
-        { id: 6, link: "./project/javascript/Waterfal_flow/index.html", innerText: "瀑布流" },
-        { id: 7, link: "./project/react/demo1/index.html", innerText: "react-demo" },
-        { id: 8, link: "./project/vue/html5api/index.html", innerText: "vue-demo" },
-        { id: 9, link: "./project/canvas/radar/index.html", innerText: "radar" },
-        { id: 10, link: "./project/javascript/quickMenu/index.html", innerText: "quickMenu" },
-        { id: 11, link: "./project/javascript/createzushi/index.html", innerText: "注释" },
+        { id: 13, link: "./project/pano2vr/1-1/index.html", innerText: "3d室内全景" },
         { id: 12, link: "./project/canvas/radar2/index.html", innerText: "radar2" },
-        { id: 13, link: "javascript:;", innerText: "敬请期待" }
+        { id: 11, link: "./project/javascript/createzushi/index.html", innerText: "注释" },
+        { id: 10, link: "./project/javascript/quickMenu/index.html", innerText: "quickMenu" },
+        { id: 9, link: "./project/canvas/radar/index.html", innerText: "radar" },
+        { id: 8, link: "./project/vue/html5api/index.html", innerText: "vue-demo" },
+        { id: 7, link: "./project/react/demo1/index.html", innerText: "react-demo" },
+        { id: 6, link: "./project/javascript/Waterfal_flow/index.html", innerText: "瀑布流" },
+        { id: 5, link: "./project/javascript/zeptoPhotoClip/index.html", innerText: "图片裁剪" },
+        { id: 4, link: "./project/javascript/upload_images/index.html", innerText: "上传图片" },
+        { id: 3, link: "./project/javascript/countDown/index.html", innerText: "倒计时" },
+        { id: 2, link: "./project/javascript/data_select/index.html", innerText: "日期联动下拉" },
+        { id: 1, link: "./project/canvas/circel_progress/index.html", innerText: "圆形进度条" },
+        { id: 0, link: "./project/canvas/tablet/index.html", innerText: "写字板", top: "true" },
+        { id: 99999, link: "javascript:;", innerText: "敬请期待" }
     ];
     module.exports.data = indexDate;
 })
